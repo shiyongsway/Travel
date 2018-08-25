563df0d49b9994fd68e40307a4812fb4d2e365b0
Your Name <you@example.com>
2018-08-25 09:22:58 +0800

    project init

---
+++ Travel/src/pages/home/home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div >Hello World!</div>
+  </div>
+</template>
+<script>
+    export default {
+
+      data(){
+        name: "home"
+      },
+      methods:{
+
+      }
+    }
+</script>
+<style scoped>
+ 
+</style>
