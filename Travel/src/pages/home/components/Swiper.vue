fe7c707cb1c7f3979ff46569bff43624098ea23d
Your Name <you@example.com>
2018-08-28 14:51:19 +0800

    add Slide show picture

---
+++ Travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl"></img>
+      </swiper-slide>
+
+      <!-- Optional controls -->
+      <div class="swiper-pagination" slot="pagination"></div><!--显示页面图中的小点-->
+    </swiper>
+  </div>
+
+
+</template>
+
+<script>
+  export default {
+    name: "MySwiper",
+    data: function () {
+      return {
+        swiperOption: {
+          pagination:{
+            el:'.swiper-pagination',
+          },
+          loop:true,
+        },
+        swiperList:[
+          {
+            id:'0001',
+            imgUrl:'http://img1.qunarzz.com/qs/1808/e7/d11912e9c7f7c802.jpg'
+          },{
+            id:'0002',
+            imgUrl:'http://img1.qunarzz.com/qs/1808/c8/ec5d00a4e0848602.jpg'
+          }
+        ]
+      }
+    },
+  }
+
+
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>>  .swiper-pagination-bullet-active
+    background-color :#ffffff !important
+  .wrapper
+    overflow:hidden
+    width :100%
+    height :0
+    padding-bottom :31.25%
+    .swiper-pagination-bullet-active
+      background-color :red !important
+    .swiper-img
+      width:100%
+
+</style>
--- Travel/src/pages/home/home.vue
+++ Travel/src/pages/home/home.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
     <HomeHeader></HomeHeader>
+    <my-swiper></my-swiper>
+    <div>Test</div>
   </div>
 </template>
 <script>
   import HomeHeader from './components/header'
-
+  import MySwiper from './components/Swiper'
   export default {
       name:'Home',
     components:{
-      HomeHeader
+      HomeHeader,
+      MySwiper
     },
-      data(){
-        name: "home"
-      },
       methods:{
 
       }
